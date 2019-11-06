7412007f137fd933b1991be047d0ebe47d2d8a00
Amanda Treutler <amandathedev@gmail.com>
2019-11-06 08:05:27 -0800

    initialr

--- src/App.js
+++ src/App.js
@@ -1,24 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Grid, Segment } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid columns="equal">
+        <Grid.Row>
+          <Grid.Column>
+            <Segment>1</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>2</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>3</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>4</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>5</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>6</Segment>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <Segment>1</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>2</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>3</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>4</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>5</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>6</Segment>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <Segment>1</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>2</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>3</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>4</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>5</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>6</Segment>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <Segment>1</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>2</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>3</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>4</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>5</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>6</Segment>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <Segment>1</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>2</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>3</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>4</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>5</Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>6</Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
   );
 }
